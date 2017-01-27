26117ea6c8009cedb7fa0998ba8ab76a00100cf4
edgebg <ivica.doshic@gmail.com>
2017-01-27 18:16:51 +0100

    male izmene

--- src/org/edgebg/glavna/Glavna.java
+++ src/org/edgebg/glavna/Glavna.java
@@ -3,7 +3,8 @@ package org.edgebg.glavna;
 public class Glavna {
 
     public static void main(String[] args) {
-        System.out.println("Dobar dan!");
+    	System.out.println("Dobar dan!");
+    	System.out.println("Dovidjenja!");
     }
 
 }
